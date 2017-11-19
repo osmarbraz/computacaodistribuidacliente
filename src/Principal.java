e57a5b91fa345e441ae5dbf632b23c8ecc855d54
admin <admin@10.80.64.183>
2017-11-19 09:08:04 -0200

    Comentários

--- src/Principal.java
+++ src/Principal.java
@@ -2,8 +2,9 @@
 import controle.CtrCliente;
 
 /**
- * Classe que possui a operação main da aplicação. Server para dar inicio ao
- * sistema.
+ * Classe que possui a operação main da aplicação. 
+ * 
+ * Serve para dar ínicio ao sistema.
  */
 public class Principal {
 
